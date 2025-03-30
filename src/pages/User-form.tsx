4f00cfd2365f98051028759a95cb2e0e9eadb5da
ruann3res <ruanpablo1218@gmail.com>
2025-03-30 19:33:41 -0300

    feat: fix phone length mask

--- src/pages/User-form.tsx
+++ src/pages/User-form.tsx
@@ -171,14 +171,14 @@ const UserForm = () => {
                   />
                   <FormField
                     control={form.control}
-                    name="telefone"
+                    name="telefone" 
                     render={({ field }) => (
                       <FormItem>
                         <FormControl>
                           <PatternFormat
-                            format="############"
-                            mask="_"
-                            placeholder="Telefone (ex: 553496671414)"
+                            format="## ## ########"
+                            mask=" "
+                            placeholder="Telefone (ex: 55 34 96671414)"
                             value={field.value}
                             onValueChange={(values) => field.onChange(values.value)}
                             className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
